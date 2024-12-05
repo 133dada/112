0191332a796259e223235d136dfc115493f651c8
133dada <3080867551@qq.com>
2024-12-05 16:00:19 +0800

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import NewComponent from '../components/NewComponent.vue'
+
+// 在路由配置中添加
+const routes = [
+  // 现有路由...
+  {
+    path: '/new',
+    name: 'New',
+    component: NewComponent
+  }
+] 
